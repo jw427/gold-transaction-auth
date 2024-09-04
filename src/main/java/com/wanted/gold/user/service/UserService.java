d5def79b13df96d704a2d5b92216df7af9e8021c
LeeJiWon <dlwldnjs427@naver.com>
2024-09-04 21:24:41 +0900

    feat : User Controller와 Service 생성

---
+++ src/main/java/com/wanted/gold/user/service/UserService.java
@@ -0,0 +1,9 @@
+package com.wanted.gold.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
