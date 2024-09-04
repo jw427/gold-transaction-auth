faec57e80e8f9b2b17a9c3a2223585fef90cf402
LeeJiWon <dlwldnjs427@naver.com>
2024-09-04 21:20:40 +0900

    feat : 회원 엔티티 생성

---
+++ src/main/java/com/wanted/gold/user/domain/Role.java
@@ -0,0 +1,5 @@
+package com.wanted.gold.user.domain;
+
+public enum Role {
+    ROLE_MEMBER, ROLE_ADMIN
+}
---
+++ src/main/java/com/wanted/gold/user/domain/User.java
@@ -0,0 +1,34 @@
+package com.wanted.gold.user.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(unique = true, nullable = false)
+    @Size(max = 30)
+    private String username;
+
+    @Column(nullable = false)
+    @Size(max = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
