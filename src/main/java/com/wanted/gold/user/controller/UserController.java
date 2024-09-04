790ff734087e5df6eef3c891eb03b217403ac23a
LeeJiWon <dlwldnjs427@naver.com>
2024-09-04 22:55:35 +0900

    feat : 비밀번호 유효성 어노테이션 추가

--- src/main/java/com/wanted/gold/user/controller/UserController.java
+++ src/main/java/com/wanted/gold/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.wanted.gold.user.controller;
 import com.wanted.gold.user.dto.SignUpRequestDto;
 import com.wanted.gold.user.dto.SignUpResponseDto;
 import com.wanted.gold.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto requestDto) {
+    public ResponseEntity<SignUpResponseDto> signUp(@Valid @RequestBody SignUpRequestDto requestDto) {
         SignUpResponseDto responseDto = userService.signUp(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
