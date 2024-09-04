c6e84e0015ffdb95d9cdd73196d79b0799abd95e
LeeJiWon <dlwldnjs427@naver.com>
2024-09-04 21:38:35 +0900

    feat : 비밀번호 암호화를 위한 Security 설정

---
+++ src/main/java/com/wanted/gold/user/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.wanted.gold.user.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((auth) -> auth
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+}
--- src/main/java/com/wanted/gold/user/service/UserService.java
+++ src/main/java/com/wanted/gold/user/service/UserService.java
@@ -1,9 +1,12 @@
 package com.wanted.gold.user.service;
 
+import com.wanted.gold.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
 }
