24ca80c125637b24570a3c69d3b3ae794f2b0b75
LeeJiWon <dlwldnjs427@naver.com>
2024-09-06 16:22:05 +0900

    feat : 액세스 토큰 재발급 - 액세스 토큰 재발급에 필요한 Request, Response Dto 작성 - 리프레시 토큰 에러와 관련한 에러코드 작성 - 액세스 토큰 재발급 로직 작성

--- src/main/java/com/wanted/gold/exception/ErrorCode.java
+++ src/main/java/com/wanted/gold/exception/ErrorCode.java
@@ -13,8 +13,8 @@ public enum ErrorCode {
     // 로그인
     INVALID_LOGIN_PARAMETER(HttpStatus.BAD_REQUEST, "username 또는 password가 빈 값인지 확인해주세요."),
     LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "username 또는 password를 확인해주세요. 회원가입하지 않았을 경우 회원가입을 진행해주세요."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다.");
-
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    INVALID_OR_EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "세션이 만료되었습니다. 다시 로그인해주세요.");
 
     private final HttpStatus status;
     private final String message;
