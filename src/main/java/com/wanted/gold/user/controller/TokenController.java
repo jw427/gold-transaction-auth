2ecc026a372f5999d0de37d699ef45549622b08c
LeeJiWon <dlwldnjs427@naver.com>
2024-09-06 16:48:58 +0900

    refactor : 토큰 재발급 경로 접근 권한 수정

--- src/main/java/com/wanted/gold/user/controller/TokenController.java
+++ src/main/java/com/wanted/gold/user/controller/TokenController.java
@@ -1,2 +1,25 @@
-package com.wanted.gold.user.controller;public class TokenController {
+package com.wanted.gold.user.controller;
+
+import com.wanted.gold.user.dto.TokenRequestDto;
+import com.wanted.gold.user.dto.TokenResponseDto;
+import com.wanted.gold.user.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tokens")
+@RequiredArgsConstructor
+public class TokenController {
+    private final TokenService tokenService;
+
+    @PostMapping("/access-token")
+    public ResponseEntity<TokenResponseDto> createNewAccessToken(@RequestBody TokenRequestDto requestDto) {
+        TokenResponseDto responseDto = tokenService.createNewAccessToken(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
 }
