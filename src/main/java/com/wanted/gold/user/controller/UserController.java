19bb428b423086f42b0bf2a4fe6cf40751e4139e
LeeJiWon <dlwldnjs427@naver.com>
2024-09-11 13:18:08 +0900

    feat : Swagger 설정 - Swagger 설정 및 API 문서화

--- src/main/java/com/wanted/gold/user/controller/UserController.java
+++ src/main/java/com/wanted/gold/user/controller/UserController.java
@@ -5,6 +5,11 @@ import com.wanted.gold.user.dto.SignUpResponseDto;
 import com.wanted.gold.user.dto.UserLoginRequestDto;
 import com.wanted.gold.user.dto.UserLoginResponseDto;
 import com.wanted.gold.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,11 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(name = "User", description = "User API")
 public class UserController {
     private final UserService userService;
 
     // 회원가입
     @PostMapping("/sign-up")
+    @Operation(summary = "회원가입", description = "회원가입 시 사용하는 API")
+    @ApiResponse(responseCode = "201", description = "Created",
+            content = {@Content(schema = @Schema(implementation = SignUpResponseDto.class))})
     public ResponseEntity<SignUpResponseDto> signUp(@Valid @RequestBody SignUpRequestDto requestDto) {
         SignUpResponseDto responseDto = userService.signUp(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
@@ -29,6 +38,9 @@ public class UserController {
 
     // 로그인
     @PostMapping("/login")
+    @Operation(summary = "로그인", description = "로그인 시 사용하는 API")
+    @ApiResponse(responseCode = "200", description = "OK",
+            content = {@Content(schema = @Schema(implementation = UserLoginResponseDto.class))})
     public ResponseEntity<UserLoginResponseDto> login(@RequestBody UserLoginRequestDto requestDto) {
         UserLoginResponseDto responseDto = userService.login(requestDto);
         return ResponseEntity.ok().body(responseDto);
